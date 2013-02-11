dc6e17bd1bd5628495e8ada412ec71ac7fe087a5
Yorkie Chadwick <y.chadwick@networking.ch>
2013-02-11 09:14:20 +0100

    use the symfony reverse caching

--- web/app.php
+++ web/app.php
@@ -13,12 +13,18 @@ $loader->register(true);
 */
 
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+// wrap the default AppKernel with the AppCache one
+$kernel = new AppCache($kernel);
+$kernel->handle(Request::createFromGlobals())->send();
+error_log($kernel->getLog());
+//$kernel = new AppKernel('prod', false);
+//$kernel->loadClassCache();
+////$kernel = new AppCache($kernel);
+//$request = Request::createFromGlobals();
+//$response = $kernel->handle($request);
+//$response->send();
+//$kernel->terminate($request, $response);
