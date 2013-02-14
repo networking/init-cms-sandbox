d6f0b626fa4dd89c9413057b12c73c01c8567c5b
Yorkie Chadwick <y.chadwick@networking.ch>
2013-02-14 19:04:26 +0100

    updated lock and app kernel to include iBrows SonataAdminAnnotationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -44,7 +44,8 @@ class AppKernel extends Kernel
 			new Networking\MediaBundle\NetworkingMediaBundle(),
 			new Networking\GalleryBundle\NetworkingGalleryBundle(),
             new Sandbox\InitCmsBundle\SandboxInitCmsBundle(),
-            new Ivory\CKEditorBundle\IvoryCKEditorBundle()
+            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+            new Ibrows\Bundle\SonataAdminAnnotationBundle\IbrowsSonataAdminAnnotationBundle(),
 		);
 
 		if (in_array($this->getEnvironment(), array('dev', 'test')))
