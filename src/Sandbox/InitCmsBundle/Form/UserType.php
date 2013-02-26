509fefe9e0d1ba4e5395fea960c3c598dd1c8355
Yorkie Chadwick <yorkie@me.com>
2013-02-26 22:58:47 +0100

    added setup assistant for the CMS

---
+++ src/Sandbox/InitCmsBundle/Form/UserType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sandbox\InitCmsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username', 'text');
+        $builder->add('email', 'email');
+        $builder->add('password', 'password');
+    }
+
+    public function getName()
+    {
+        return 'user';
+    }
+}
