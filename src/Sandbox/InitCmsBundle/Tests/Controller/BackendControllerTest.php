c40171c298903512ae11a5b6a8fc687b09f86554
Sonja Brodersen <s.brodersen@networking.ch>
2013-02-28 14:54:13 +0100

    added some tests for backend language control

--- src/Sandbox/InitCmsBundle/Tests/Controller/BackendControllerTest.php
+++ src/Sandbox/InitCmsBundle/Tests/Controller/BackendControllerTest.php
@@ -21,7 +21,7 @@ class BackendControllerTest extends WebTestCase
     {
         restoreDatabase();
         $client = static::createClient();
-        $crawler = $client->request('GET', '/admin/login', array('_locale' => 'en_US'));
+        $crawler = $client->request('GET', '/admin/login', array('admin/_locale' => 'en_US'));
         $this->assertEquals(200, $client->getResponse()->getStatusCode(), 'no further redirect');
 
         // try login with unknown user
@@ -41,7 +41,7 @@ class BackendControllerTest extends WebTestCase
     {
         restoreDatabase();
         $client = static::createClient();
-        $crawler = $client->request('GET', '/admin/login', array('_locale' => 'en_US'));
+        $crawler = $client->request('GET', '/admin/login', array('admin/_locale' => 'en_US'));
         $this->assertEquals(200, $client->getResponse()->getStatusCode(), 'no further redirect');
 
         // try login with correct user
@@ -57,4 +57,94 @@ class BackendControllerTest extends WebTestCase
         $this->assertTrue($crawler->filter('html:contains("Dashboard")')->count() > 0, 'dashboard');
 
     }
+
+    public function testBackendLanguage()
+    {
+        restoreDatabase();
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/admin/login');
+        // try login with correct user
+        $form = $crawler->filter('form')->form();
+        // set some values
+        $form['_username'] = 'foo';
+        $form['_password'] = 'bar';
+        // submit the form
+        $crawler = $client->submit($form);
+        $crawler = $client->followRedirect();
+        $this->assertTrue($crawler->filter('html:contains("Dashboard")')->count() > 0, 'dashboard');
+
+        // edit user
+        $crawler = $client->request('GET', '/admin/cms/users/1/edit');
+        $form = $crawler->filter('form')->form();
+        // get the unique key
+        $key = array_keys($form->getPhpValues());
+        // set some values
+        $form[$key[0].'[locale]'] = 'de_CH';
+        // submit the form
+        $crawler = $client->submit($form);
+        // logout
+        $crawler = $client->request('GET', '/admin/logout');
+
+
+        // login
+        $crawler = $client->request('GET', '/admin/login');
+
+        // login again
+        $form = $crawler->filter('form')->form();
+        // set some values
+        $form['_username'] = 'foo';
+        $form['_password'] = 'bar';
+        // submit the form
+        $crawler = $client->submit($form);
+        $crawler = $client->followRedirect();
+
+        // is the locale de?
+        $this->assertTrue($crawler->filter('a:contains("abmelden")')->count() > 0, 'it\'s German');
+
+    }
+
+    public function testBackendLanguage2()
+    {
+        restoreDatabase();
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/admin/login');
+        // try login with correct user
+        $form = $crawler->filter('form')->form();
+        // set some values
+        $form['_username'] = 'foo';
+        $form['_password'] = 'bar';
+        // submit the form
+        $crawler = $client->submit($form);
+        $crawler = $client->followRedirect();
+        $this->assertTrue($crawler->filter('html:contains("Dashboard")')->count() > 0, 'dashboard');
+
+
+        $crawler = $client->request('GET', '/admin/cms/users/1/edit');
+        // edit form
+        $form = $crawler->filter('form')->form();
+        // get form unique key
+        $key = array_keys($form->getPhpValues());
+        // set some values
+        $form[$key[0].'[locale]'] = 'en_US';
+        // submit the form
+        $crawler = $client->submit($form);
+
+        // logout
+        $crawler = $client->request('GET', '/admin/logout');
+
+        // login again
+        $crawler = $client->request('GET', '/admin/login');
+        // try to login
+        $form = $crawler->filter('form')->form();
+        // set some values
+        $form['_username'] = 'foo';
+        $form['_password'] = 'bar';
+        // submit the form
+        $crawler = $client->submit($form);
+        $crawler = $client->followRedirect();
+        // is the locale de?
+        $this->assertTrue($crawler->filter('a:contains("Logout")')->count() > 0, 'It\'s English');
+
+    }
+
 }
