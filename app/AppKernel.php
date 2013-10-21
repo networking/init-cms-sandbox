cf0643acf3ae8c52221d64730f0be96e97703cee
Yorkie Chadwick <y.chadwick@networking.ch>
2013-10-21 14:21:44 +0200

    added new Application folder and updated config

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,53 +5,60 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
-	public function registerBundles()
-	{
-		$bundles = array(
-			new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-			new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-			new Symfony\Bundle\TwigBundle\TwigBundle(),
-			new Symfony\Bundle\MonologBundle\MonologBundle(),
-			new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-			new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-			new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-			new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-			new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-			new JMS\AopBundle\JMSAopBundle(),
-			new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-			new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-			new JMS\SerializerBundle\JMSSerializerBundle($this),
-			new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-			new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
-			new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
-			new FOS\UserBundle\FOSUserBundle(),
-			new Sonata\AdminBundle\SonataAdminBundle(),
-			new Sonata\jQueryBundle\SonatajQueryBundle(),
-			new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
-			new Sonata\BlockBundle\SonataBlockBundle(),
-			new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
-			new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
-			new Sonata\MediaBundle\SonataMediaBundle(),
-			new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-			new Symfony\Cmf\Bundle\RoutingExtraBundle\SymfonyCmfRoutingExtraBundle(),
-			new Networking\InitCmsBundle\NetworkingInitCmsBundle(),
-            new Sandbox\InitCmsBundle\SandboxInitCmsBundle(),
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle($this),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+            new Sonata\MediaBundle\SonataMediaBundle(),
+            new \Sonata\NewsBundle\SonataNewsBundle(),
+            new Sonata\MarkItUpBundle\SonataMarkItUpBundle(),
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+            new \Sonata\FormatterBundle\SonataFormatterBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
+            new Networking\InitCmsBundle\NetworkingInitCmsBundle(),
+//            new Sandbox\InitCmsBundle\SandboxInitCmsBundle(),
             new Ibrows\Bundle\SonataAdminAnnotationBundle\IbrowsSonataAdminAnnotationBundle(),
-		);
+            new Lexik\Bundle\TranslationBundle\LexikTranslationBundle(),
+            new Ibrows\SonataTranslationBundle\IbrowsSonataTranslationBundle(),
+            new Application\Networking\InitCmsBundle\ApplicationNetworkingInitCmsBundle(),
+        );
 
-		if (in_array($this->getEnvironment(), array('dev', 'test')))
-		{
-			$bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
-			$bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-			$bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-			$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-		}
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
 
-		return $bundles;
-	}
+        return $bundles;
+    }
 
-	public function registerContainerConfiguration(LoaderInterface $loader)
-	{
-		$loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
-	}
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
+    }
 }
