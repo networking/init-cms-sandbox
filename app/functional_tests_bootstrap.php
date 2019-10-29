713cec3c56b68c81ce1b3a68fcba2cff4e9d1cf8
Yorkie Chadwick <ychadwick@gmail.com>
2019-10-29 17:25:57 +0100

    @WIP symfony upgrade

--- app/functional_tests_bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-include_once __DIR__.'/../vendor/autoload.php';
-include_once __DIR__ . '/../var/bootstrap.php.cache';
-
-
-require_once __DIR__ . '/AppKernel.php';
---
+++ src/Tests/Fixtures/LoadPages.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * This file is part of the Networking package.
+ *
+ * (c) net working AG <info@networking.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Fixtures;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Networking\InitCmsBundle\Model\PageInterface;
+/**
+ * Class LoadPages
+ * @package App\Tests\Fixtures
+ * @author Yorkie Chadwick <y.chadwick@networking.ch>
+ */
+class LoadPages extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface $container
+     */
+    private $container;
+
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param \Doctrine\Common\Persistence\ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $languages = $this->container->getParameter('networking_init_cms.page.languages');
+
+        foreach ($languages as $key => $lang) {
+            $this->createHomePages($manager, $lang['locale'], $key, $languages);
+        }
+    }
+
+    /**
+     * @param \Doctrine\Common\Persistence\ObjectManager $manager
+     * @param string $locale
+     * @param integer $key
+     * @param array $languages
+     */
+    public function createHomePages(ObjectManager $manager, $locale, $key, $languages)
+    {
+        $pageClass = $this->container->getParameter('networking_init_cms.admin.page.class');
+        /** @var PageInterface $homePage */
+        $homePage = new $pageClass;
+
+        $homePage->setLocale($locale);
+        $homePage->setPageName('Homepage ' . $locale);
+        $homePage->setMetaTitle('Homepage '.$locale);
+        $homePage->setMetaKeyword('homepage');
+        $homePage->setMetaDescription('This is the homepage');
+        $homePage->setStatus(PageInterface::STATUS_PUBLISHED);
+        $homePage->setIsHome(true);
+        $homePage->setTemplateName($this->getFirstTemplate());
+        $homePage->setActiveFrom(new \DateTime('-1 day'));
+        $homePage->setActiveTo(new \DateTime('+1 day'));
+
+        // set original for translations
+        if($key > 0)
+        {
+            $firstPage = $this->getReference('homepage_'.$languages['0']['locale']);
+            $homePage->setOriginal($firstPage);
+        }
+
+        $manager->persist($homePage);
+        $manager->flush();
+
+        $this->addReference('homepage_'.$locale, $homePage);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getFirstTemplate()
+    {
+        $templates = $this->container->getParameter('networking_init_cms.page.templates');
+
+        foreach ($templates as $key => $template) {
+            return $key;
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+}
